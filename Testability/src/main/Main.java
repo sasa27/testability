494c02cfb0b2df77c358723bac706e3a1c6d31d2
Elliott Blot <elliott.blot@gmail.com>
2019-11-04 11:16:21 +0100

    add RESULT in the example

--- Testability/src/main/Main.java
+++ Testability/src/main/Main.java
@@ -45,18 +45,22 @@ public class Main {
 					}
 					union.addAll(t.getOt());
 				}
-				else {
+				else if (t.isInput()){
 					exclu.addAll(t.getOt());
 				}
 			}
-			union.removeAll(exclu);
-			if (card1 && (!union.isEmpty()) && (!union.contains("nope"))) {
+			HashSet<String> set = new HashSet<String>();
+			set.addAll(union);
+			set.removeAll(exclu);
+			if (card1 && (!set.isEmpty()) && (!set.contains("nope"))) {
 				obs++;
 			}
 			else {
 				System.out.println("issue on input: " + in + "\n"
 						+ "all card equals 1:" + card1 + "\n"
-						+ "set = " + union + "\n");
+						+ "set = " + set + "\n"
+						+ "union = " + union + "\n"
+						+ "excluded = " + exclu + "\n");
 			}
 		}
 		return obs / (double) model.getInputs().size();
@@ -76,18 +80,26 @@ public class Main {
 					}
 					union.addAll(t.getOt());
 				}
-				else {
+				else if(!t.isInput()) {
+					/*if (t.getOt().contains("?/json.htm(Host=192.168.43.101;Dest=192.168.43.76;Verb=GET;Uri=/json.htm;type=command;param=udevice;idx=1;nvalue;svalue=23.00;62.00;3;rssi)")){
+						System.out.println(t);
+					}*/
 					exclu.addAll(t.getOt());
 				}
 			}
-			union.removeAll(exclu);
-			if (card1 && (!union.isEmpty()) && (!union.contains("nope"))) {
+			HashSet<String> set = new HashSet<String>();
+			set.addAll(union);
+			set.removeAll(exclu);
+			if (card1 && (!set.isEmpty()) && (!set.contains("nope"))) {
 				ct++;
 			}
 			else {
 				System.out.println("issue on output: " + out + "\n"
 						+ "all card equals 1:" + card1 + "\n"
-						+ "set = " + union + "\n");
+						+ "set = " + set + "\n"
+						+ "union = " + union + "\n"
+						+ "excluded = " + exclu + "\n");
+				
 			}
 		}
 		return ct / (double) model.getOutputs().size();
